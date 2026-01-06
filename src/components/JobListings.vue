5994f6897dadb3b04e180f34813e096adcfd7545
DANUCKSAN <sathiyarajdanucksan@gmail.com>
2026-01-06 14:51:22 +1100

    day-2

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -1,18 +1,28 @@
 <script setup>
-import jobData from '@/jobs.json';
-import { ref } from 'vue';
+
+import { reactive } from 'vue';
 import JobListing from '@/components/JobListing.vue';
-import { defineComponent } from 'vue';
+import { defineComponent,onMounted } from 'vue';
+import {RouterLink} from 'vue-router';
+import axios from 'axios';
+import PulseLoader from 'vue-spinner/src/PulseLoader.vue';
 
 
-const jobs = ref(jobData);
-defineProps({
-  limit:Number,
-  showButton:{
-    type:Boolean,
-    default:false
+const state = reactive({
+  jobs: [],
+  isLoading:true
+});
+
+onMounted(async () => {
+  try {
+    const response = await axios.get('http://localhost:8000/jobs');
+    state.jobs = response.data;
+  } catch (error) {
+    console.error("Error fetching jobs:", error);
+  } finally {
+    state.isLoading = false;
   }
-})
+});
 
 </script>
 
@@ -20,16 +30,24 @@ defineProps({
   <section class="bg-blue-50 px-5 px-10">
     <div class="container-xl lg:container m-auto">
         <h2 class="text-3xl font-bold text-green mb-6 text-center">Browse Jobs</h2>
-        <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-          <JobListing v-for="job in jobs.slice(0, limit || jobs.length )" :key="job.id" :job="job" />
+        <!--show loading spinner-->
+
+        <div v-if="state.isLoading" class="text-center text-gray-500 py-6">
+          <PulseLoader/>
+        </div>
+
+        <div v-else class="grid grid-cols-1 md:grid-cols-3 gap-6">
+      
+          <JobListing v-for="job in state.jobs.slice(0, limit || state.jobs.length )" :key="job.id" :job="job" />
         </div>
     </div>
   </section>
+
   <section v-if="showButton" class="m-auto max-w-lg my-10 px-6">
-      <a
-        href="/jobs"
+      <RouterLink
+        to="/jobs"
         class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >View All Jobs</a
+        >View All Jobs</RouterLink
       >
     </section>
    
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter,createWebHistory } from "vue-router";
 import HomeView from '@/views/HomeView.vue';
+import JobsView from '@/components/JobsView.vue';
+import NotFoundView from '@/views/NotFoundView.vue';
+import JobView from "@/views/JobView.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +11,23 @@ const router = createRouter({
             path: '/',
             name: 'home',
             component: HomeView
+        },
+        {
+            path: '/jobs',
+            name: 'jobs',
+            component: JobsView
+        },
+        {
+            path: '/job/:id',
+            name: 'job',
+            component: JobView,
+            props: true
+
+        },
+        {
+            path:'/:catchAll(.*)*',
+            name:'not-found',
+            component:NotFoundView
         }
     ]
 });
